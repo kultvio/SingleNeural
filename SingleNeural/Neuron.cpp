bfe430225581ed2b687c67aea1b73c735ba1e63f
kultvio <deromirk@gamil.com>
2022-12-07 15:00:49 +1000

    исправлена ошибка, при которой нейрон не работал, если input > output

--- SingleNeural/Neuron.cpp
+++ SingleNeural/Neuron.cpp
@@ -21,8 +21,9 @@ void Neuron::Train(double input, double expecterResult)
 {
 	double actualResult = input * _weight;
 	_lastError = expecterResult - actualResult;
-	double correction = _lastError / actualResult;
-	_weight += correction * smoothing;
+	double correction = _lastError / actualResult * smoothing;
+	_weight += correction;
+	if (_weight < 0) _weight = -_weight;
 }
 
 double Neuron::GetLastError()
--- SingleNeural/main.cpp
+++ SingleNeural/main.cpp
@@ -4,17 +4,18 @@
 int main()
 {
     srand(time(NULL));
-    double input = 131;
-    double output = 8287.73;
+    double input = 8287.73;
+    double output = 131;
     Neuron neuron;
-    neuron.smoothing = 0.1;
+    neuron.smoothing = 0.0001;
     int i = 1;
     while(neuron.GetLastError() > neuron.smoothing || neuron.GetLastError() < (-neuron.smoothing) )
     {
         neuron.Train(input, output);
-        if (i % 100 == 0) std::cout << neuron.GetLastError() << std::endl;
-        i++;
+        //if (i % 1000000 == 0) std::cout << neuron.GetLastError() << std::endl;
+        // i++;
     }
-    std::cout << neuron.ProcessInputData(1000);
+    std::cout << neuron.ProcessInputData(1000) << std::endl;;
+    std::cout << neuron.RestoreInputData(1000) << std::endl;;
 }
 
